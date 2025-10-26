8b4a119f9a7195baac3c31eecc70fdc0553a19c4
Ahmed-Mansy <Ahmedalimansy1@gmail.com>
2025-10-26 17:33:00 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-projects',
@@ -7,5 +8,72 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+
+  categories = ['All Projects', 'Java Script', 'Angular', 'JQuery', 'Gaming'];
+
+  selectedCategory = 'All Projects';
+  render = true;
+  fading = false; // new state for fade animation
+  loading = false;
+
+  photos = [
+    { path: 'https://crepiano.netlify.app/', picUrl: '/images/projects/crepe2.jpg', category: 'Java Script' },
+    { path: 'https://github.com/Ahmed-Mansy/social-app', picUrl: '/images/projects/social.jpg', category: 'Angular' },
+    { path: 'https://e-commerce-three-sepia-uosxtn5ely.vercel.app/login', picUrl: '/images/projects/ecommerce.JPG', category: 'Angular' },
+    { path: 'https://ahmed-mansy.github.io/DevFolio/', picUrl: '/images/projects/devilo.JPG', category: 'Java Script' },
+    { path: 'https://ahmed-mansy.github.io/CRUD-SYSTEM/', picUrl: '/images/projects/CRUD.jpeg', category: 'Java Script' },
+    { path: 'https://ahmed-mansy.github.io/Movies/', picUrl: '/images/projects/movies.JPG', category: 'JQuery' },
+    { path: 'https://karamfund.pythonanywhere.com/', picUrl: '/images/projects/karam-fund.JPG', category: 'JQuery' },
+
+  ];
+
+  // Filtered photos getter
+  get filteredPhotos() {
+    if (this.selectedCategory === 'All Projects') return this.photos;
+    return this.photos.filter(p => p.category === this.selectedCategory);
+  }
+
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+    });
+  }
+
+  ngAfterViewInit() {
+    // Ensures dynamic content or route changes are recognized
+    AOS.refresh();
+  }
+
+
+
+  selectCategory(category: string) {
+    if (category === this.selectedCategory) return;
+
+    this.fading = true;
+    this.loading = true;
+
+    // fade out
+    setTimeout(() => {
+      this.selectedCategory = category;
+      this.render = false;
+
+      // re-render new photos
+      setTimeout(() => {
+        this.render = true;
+        this.fading = false;
+
+        // hide spinner after fade-in
+        setTimeout(() => {
+          this.loading = false;
+          AOS.refreshHard();
+        }, 400);
+      }, 100);
+    }, 400);
+  }
+
+
 
 }
